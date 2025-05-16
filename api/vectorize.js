c87583de7fe815729508cdc6647160c822e7dcda
Abbas Saad <49558477+AbbasSaad27@users.noreply.github.com>
2025-05-16 12:40:20 +0600

    Update vectorize.js

--- api/vectorize.js
+++ api/vectorize.js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
     try {
       const inputBuffer = Buffer.concat(buffers);
       const svg = await vectorize(inputBuffer);
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader("Content-Type", "image/svg+xml");
       res.status(200).send(svg);
     } catch (err) {
